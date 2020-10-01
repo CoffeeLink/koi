a9dc7412799f45ca2513f2d42636e05c9fd1719f
CoffeeLink <idangws14@gmail.com>
2020-10-01 19:42:03 +0700

    feat(ui): component filter price

---
+++ components/FilterPrice.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <CardFilter
+      title="Harga"
+      type="input"
+      inputtype="number"
+      :valueinput="(input) => priceinput(input)"
+    />
+  </v-container>
+</template>
+
+<script>
+import CardFilter from './CardFilter'
+
+export default {
+  components: {
+    CardFilter
+  },
+  props: {
+    priceinput: {
+      type: Function,
+      default: () => ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
