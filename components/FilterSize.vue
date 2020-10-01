c14e46317ddf627cf228ae95f308656fa8e53b10
CoffeeLink <idangws14@gmail.com>
2020-10-01 19:42:19 +0700

    feat(ui): component filter size

---
+++ components/FilterSize.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <CardFilter
+      title="Ukuran"
+      type="select"
+      :listoptions="optionsize"
+      :valueinput="(input) => sizeinput(input)"
+    />
+  </v-container>
+</template>
+
+<script>
+import CardFilter from './CardFilter'
+
+export default {
+  name: 'FilterSize',
+  components: {
+    CardFilter
+  },
+  props: {
+    sizeinput: {
+      type: Function,
+      default: () => ''
+    },
+    optionsize: {
+      type: Array,
+      default: () => ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
