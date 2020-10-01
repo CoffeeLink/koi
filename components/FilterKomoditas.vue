36d6680335774355bfcdd89335d22b7a4ff4550e
CoffeeLink <idangws14@gmail.com>
2020-10-01 19:41:25 +0700

    feat(ui): filter komoditas

---
+++ components/FilterKomoditas.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <CardFilter
+      title="Komoditas"
+      type="input"
+      :valueinput="(input) => komoditasinput(input)"
+    />
+  </v-container>
+</template>
+
+<script>
+import CardFilter from './CardFilter'
+
+export default {
+  components: {
+    CardFilter
+  },
+  props: {
+    komoditasinput: {
+      type: Function,
+      default: () => ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
