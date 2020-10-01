1eb2698caed4695384ef29c2c3153459b2f33d95
CoffeeLink <idangws14@gmail.com>
2020-10-01 19:41:46 +0700

    feat(ui): component filter location

---
+++ components/FilterLocations.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container>
+    <CardFilter
+      title="Provinsi"
+      type="select"
+      :listoptions="optionprovinsi"
+      :valueinput="(input) => porvinsiinput(input)"
+    />
+    <CardFilter
+      title="Kota"
+      type="select"
+      :listoptions="optionkota"
+      :valueinput="(input) => kotainput(input)"
+    />
+  </v-container>
+</template>
+
+<script>
+import CardFilter from './CardFilter'
+
+export default {
+  components: {
+    CardFilter
+  },
+  props: {
+    porvinsiinput: {
+      type: Function,
+      default: () => ''
+    },
+    kotainput: {
+      type: Function,
+      default: () => ''
+    },
+    optionprovinsi: {
+      type: Array,
+      default: () => []
+    },
+    optionkota: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
